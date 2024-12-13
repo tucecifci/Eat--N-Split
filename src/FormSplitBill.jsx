fddbea2e37f0294698248f1565242ec2152e18af
tucecifci <tucecifcii@gmail.com>
2024-12-13 16:37:49 +0300

    selected friend feat added

--- src/FormSplitBill.jsx
+++ src/FormSplitBill.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 
-function FormSplitBill() {
+function FormSplitBill({selectedFriend}) {
   return (
     <form className="form-split-bill">
-      <h2>Split a bill with X</h2>
+      <h2>Split a bill with {selectedFriend.name} </h2>
 
       <label>💰 Bill Value</label>
       <input type="text" />
       <label>👩🏻 Your Expense</label>
       <input type="text" />
-      <label>👫 X's Expense</label>
+      <label>👫 {selectedFriend.name} Expense</label>
       <input type="text" disabled />
 
       <label>💸 Who is paying the bill</label>
       <select>
         <option value="user">You</option>
-        <option value="friend">X</option>
+        <option value="friend">{selectedFriend.name} </option>
       </select>
 
       <button className="button">Split Bill</button>
